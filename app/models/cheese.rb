94b201df93fc4f637b48d51fc21ca695b610b5b8
Michael <michael.daniels1013@gmail.com>
2022-12-08 22:08:34 -0600

    commit tree 7d523f7c7a6c3c5434d14ebf62bb9d72b88d953f

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -4,4 +4,18 @@ class Cheese < ApplicationRecord
     "#{name}: $#{price}"
   end
 
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+  end
+
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  end
+
 end
